91976ff92ce5f956e4bd9f887fd46d41b74f654e
martynasbaukys <martynas.baukys@gmail.com>
2022-11-29 19:06:08 +0200

    h

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,32 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+
+	private WebDriver driver;
+
+	@Test
+	public void testEasy() {
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		System.out.println(title + " VEIKIA");
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Marbau\\eclipse-workspace\\MainFrame\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
