1a18c097e63de4ce9f48e58f575813d79d437b3e
martis915 <martynasbaukys@gmail.com>
2022-12-03 23:34:02 +0200

    Update NewTest.java

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -28,7 +28,6 @@ public class NewTest {
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		options.addArguments("disable-infobars"); // disabling infobars
 		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		options.addArguments("--no-sandbox"); // Bypass OS security model
 		driver = new ChromeDriver(options);
