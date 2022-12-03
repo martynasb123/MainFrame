3a0eec44251bd69c1b5a44e860bacf0ea2961e1f
martis915 <martynasbaukys@gmail.com>
2022-12-03 23:04:39 +0200

    fasf

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -2,6 +2,7 @@ package example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -23,6 +24,13 @@ public class NewTest {
 	@BeforeTest
 	public void beforeTest() {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ File.separator+"chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
 		driver = new ChromeDriver();
 	}
 
